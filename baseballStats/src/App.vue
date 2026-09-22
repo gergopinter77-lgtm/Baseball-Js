ddbb6d7b6a47a7491994c85e2f0dae9902fbf6ba
gergopinter77-lgtm <gergopinter77@gmail.com>
2026-09-22 22:45:18 +0200

    design updates and api added

--- baseballStats/src/App.vue
+++ baseballStats/src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Atbat from '@/components/icons/atbat.svg?component'
+
 </script>
 
 <template>
   <header>
-    <div class="flex justify-center bg-primary ">
-      <div class="text-white text-xl my-5">
-        <RouterLink to="/" class="mr-2.5  hover:text-secondary">Home</RouterLink>
-        <RouterLink to="/about" class="ml-2.5 hover:text-secondary">About</RouterLink>
-      </div>
+    <div class="bg-primary">
+      <nav class="p-5 flex items-center gap-5 text-text-nav text-xl">
+        <Atbat class="w-20 h-20" />
+        <div class="flex-1 flex justify-center gap-5">
+          <RouterLink to="/" class="hover:text-secondary">Home</RouterLink>
+          <RouterLink to="/about" class="hover:text-secondary">About</RouterLink>
+        </div>
+      </nav>
     </div>
     <div class="wrapper">
 
@@ -16,7 +21,7 @@ import { RouterLink, RouterView } from 'vue-router'
       </nav>
     </div>
   </header>
-  <div class="bg-white min-h-screen">
+  <div class="bg-linear-135 from-primary to-primary-gradient min-h-screen">
     <RouterView />
   </div>
 </template>
--- baseballStats/src/components/MainOptions.vue
+++ baseballStats/src/components/MainOptions.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center mt-2">
-        <RouterLink to="/compare" class="bg-secondary w-80 h-16 mr-px rounded-lg hover:cursor-pointer hover:bg-secondary-dark flex justify-center items-center text-white text-xl">Compare</RouterLink>
-        <RouterLink to="/standings" class="bg-secondary w-80 h-16 ml-px rounded-lg hover:cursor-pointer hover:bg-secondary-dark flex justify-center items-center text-white text-xl">Standings</RouterLink>
+        <RouterLink to="/compare" class="bg-secondary-dark w-80 h-16 mr-px rounded-lg hover:cursor-pointer hover:bg-secondary-dark flex justify-center items-center text-white text-xl">Compare</RouterLink>
+        <RouterLink to="/standings" class="bg-secondary-dark w-80 h-16 ml-px rounded-lg hover:cursor-pointer hover:bg-secondary-dark flex justify-center items-center text-white text-xl">Standings</RouterLink>
     </div>
 </template>
 
